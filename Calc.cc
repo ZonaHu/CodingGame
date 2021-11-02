ec91bb5d6e19ec2025a24b8fcf256a111fd2352e
Zonaahmmm <hzm5622@126.com>
2021-11-02 11:52:39 +0800

    Create Calc.cc

---
+++ Calc.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>       /* pow */
+
+
+using namespace std;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+int main()
+{
+    int a;
+    cin >> a; cin.ignore();
+    cout << a*(a-1) << endl;
+}
