e362ad622b03d341cb17c73ce1e106bbca88aacc
Zonaahmmm <hzm5622@126.com>
2021-11-02 11:23:25 +0800

    Create EndOfString.cc

---
+++ EndOfString.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+int main()
+{
+    string n;
+    cin >> n; cin.ignore();
+    cout << n.back() << endl;
+}
