2286ab29dc7bfe51b266af1c4e2000608e4a1b18
Zonaahmmm <hzm5622@126.com>
2021-11-02 12:21:09 +0800

    Update ShortestModeSnameCaseToCamelCase.py

--- ShortestModeSnameCaseToCamelCase.py
+++ ShortestModeSnameCaseToCamelCase.py
@@ -1,3 +1,8 @@
 a=[word.capitalize()for word in input().split('_')]
 a[0]=a[0].lower()
 print("".join(a))
+
+# Shortest Version:
+# I=input
+# a,*b=map(str.title,I().split("_"))
+# I(a.lower()+"".join(b))
